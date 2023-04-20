a68f222188a120f8e6237d154ebe6b9ac2f437bc
gtoselli4 <giantoselli.4@gmail.com>
2023-04-20 01:12:19 -0300

    DB done!

---
+++ api/src/models/Activity.js
@@ -0,0 +1,22 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+  sequelize.define('activity', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    difficulty: {
+      type: DataTypes.ENUM('1', '2', '3', '4', '5'),
+      allowNull: false,
+    },
+    duration: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    season: {
+      type: DataTypes.ENUM('Verano', 'Otoño', 'Invierno', 'Primavera'),
+      allowNull: false,
+    },
+  });
+};
--- api/src/models/Country.js
+++ api/src/models/Country.js
@@ -4,7 +4,36 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('country', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
     name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    flag: {type: DataTypes.STRING,
+    allowNull: false,
+    },
+    continents: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    capital: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    region: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    area: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    population: {
       type: DataTypes.STRING,
       allowNull: false,
     },
